4d5827a2c11badc0f7cf16f66edec53da99828b6
AlexanderFalca <x_mordred@hotmail.com>
2024-03-11 17:16:11 +0000

    create vercel config

---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default function Home(){
+    return <h1>Si. Sempre estou lembrando de ti, meu amor. Eu sei que é um privilégio poder conversar contigo e compartilhar momentos inesquecíveis. Obrigado por acreditar em mim! Te amo, meu amor perfeito. Ass: Ale</h1>
+}
