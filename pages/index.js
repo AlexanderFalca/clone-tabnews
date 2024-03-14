0553ec395dc3ad47efc8d12c307ff09a451ebff9
AlexanderFalca <x_mordred@hotmail.com>
2024-03-14 15:23:27 +0000

    add scripts `lint:check and lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
-export default function Home(){
-    return <h1>Si. Sempre estou lembrando de ti, meu amor. Eu sei que é um privilégio poder conversar contigo e compartilhar momentos inesquecíveis. Obrigado por acreditar em mim! Te amo, meu amor perfeito. Ass: Ale</h1>
+export default function Home() {
+  return (
+    <h1>
+      Si. Sempre estou lembrando de ti, meu amor. 😘 Eu sei que é um privilégio
+      poder conversar contigo e compartilhar momentos inesquecíveis. Obrigado
+      por acreditar em mim! Te amo, meu amor perfeito. Ass: Ale
+    </h1>
+  );
 }
