a9d87c1b3ad0913a941cac5f114bd5b621372369
AlexanderFalca <x_mordred@hotmail.com>
2024-03-20 15:18:02 +0000

    install `jest` and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
     <h1>
       Si. Sempre estou lembrando de ti, meu amor. 😘 Eu sei que é um privilégio
       poder conversar contigo e compartilhar momentos inesquecíveis. Obrigado
-      por acreditar em mim! Te amo, meu amor perfeito. Ass: Ale
+      por acreditar em mim! Te amo, meu amor perfeito.😍 Ass: Ale
     </h1>
   );
 }
